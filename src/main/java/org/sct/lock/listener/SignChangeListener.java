bfb1fae04c867efc65af3b1e0ce1686e408cf49c
LovesAsuna <625924077@qq.com>
2020-03-23 16:51:45 +0800

    [-] 将牌子内容改变事件中的门处理部分独立作为类

--- src/main/java/org/sct/lock/listener/SignChangeListener.java
+++ src/main/java/org/sct/lock/listener/SignChangeListener.java
@@ -8,12 +8,7 @@ import org.bukkit.event.block.SignChangeEvent;
 import org.sct.lock.data.LockData;
 import org.sct.lock.enumeration.ConfigType;
 import org.sct.lock.file.Config;
-import org.sct.lock.util.function.LocationUtil;
-import org.sct.lock.util.function.LockUtil;
-import org.sct.plugincore.util.BasicUtil;
-
-import java.util.Map;
-
+import org.sct.lock.util.function.SIgnProcessUtil;
 
 /**
  * @author LovesAsuna
@@ -33,6 +28,7 @@ public class SignChangeListener implements Listener {
         for (String doors : Config.getStringList(ConfigType.SETTING_DOORTYPE)) {
             if (LockData.getPlayerDoorLocation().get(e.getPlayer()).getBlock().getType() == Material.getMaterial(doors)) {
                 cancel = false;
+                break;
             }
         }
 
@@ -41,62 +37,7 @@ public class SignChangeListener implements Listener {
         }
 
         if (e.getPlayer() == LockData.getPlayerSignLocation().inverse().get(lt)) {
-            if (e.getLine(0).equalsIgnoreCase(Config.getString(ConfigType.SETTING_LOCKSYMBOL)) && !e.getLine(1).equalsIgnoreCase("")) {
-
-                /*如果世界不匹配,返回*/
-                if (!LocationUtil.checkWorld(e.getBlock().getLocation())) {
-                    return;
-                }
-                /*替换第一行Symbol*/
-                e.setLine(0, BasicUtil.convert(Config.getString(ConfigType.SETTING_SYMBOLREPLACE)));
-
-                /*替换第四行为玩家名*/
-                e.setLine(3,"§l" + e.getPlayer().getName());
-                if (Integer.getInteger(e.getLine(1)) != null) {
-                    int money = Integer.getInteger(e.getLine(2));
-                }
-
-                /*替换第二行价格*/
-                e.setLine(1,BasicUtil.convert(Config.getString(ConfigType.SETTING_CHARGE) + e.getLine(1)));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = e.getLine(2);
-                boolean direction = false;
-                if (line.contains(Config.getString(ConfigType.SETTING_FLAGENTER))) {
-                    buffer.append(BasicUtil.convert(Config.getString(ConfigType.SETTING_ENTERREPLACE)));
-                    direction = true;
-                }
-
-                if (line.contains(Config.getString(ConfigType.SETTING_FLAGLEAVE))) {
-                    buffer.append(BasicUtil.convert(Config.getString(ConfigType.SETTING_LEAVEREPLACE)));
-                    direction = true;
-                }
-
-                /*开启默认双向*/
-                if (!direction) {
-                    buffer.append(BasicUtil.convert(Config.getString(ConfigType.SETTING_ENTERREPLACE) + Config.getString(ConfigType.SETTING_LEAVEREPLACE)));
-                }
-
-                /*-----------------------------------------------------*/
-                if (line.contains(Config.getString(ConfigType.SETTING_FLAGEMPTY))) {
-                   buffer.append(BasicUtil.convert(Config.getString(ConfigType.SETTING_EMPTYREPLACE)));
-                }
-
-                if (line.contains(Config.getString(ConfigType.SETTING_FLAGMONEY))) {
-                    int money = BasicUtil.ExtraceInt(line);
-                    Map<String, Boolean> moneyDetail = LockUtil.getMoneydetail(line, 0, 0);
-                    String symbol = moneyDetail.keySet().iterator().next();
-                    buffer.append(BasicUtil.convert(BasicUtil.replace(Config.getString(ConfigType.SETTING_MONEYREPLACE), "%needmoney", symbol + money)));
-                }
-
-                if (line.contains(Config.getString(ConfigType.SETTING_FLAGEFFECT))) {
-                    buffer.append(BasicUtil.convert(Config.getString(ConfigType.SETTING_EFFECTREPLACE)));
-                }
-
-                e.setLine(2, buffer.toString());
-            }
-            LockData.getPlayerSignLocation().remove(e.getPlayer());
-            LockData.getPlayerDoorLocation().remove(e.getPlayer());
+            SIgnProcessUtil.processSign(e);
         }
     }
 }
---
+++ src/main/java/org/sct/lock/util/function/SIgnProcessUtil.java
@@ -0,0 +1,108 @@
+package org.sct.lock.util.function;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.block.SignChangeEvent;
+import org.sct.lock.Lock;
+import org.sct.lock.data.LockData;
+import org.sct.lock.enumeration.ConfigType;
+import org.sct.lock.file.Config;
+import org.sct.plugincore.util.BasicUtil;
+
+import java.util.Map;
+
+/**
+ * @author LovesAsuna
+ * @date 2020/3/23 14:06
+ */
+
+public class SIgnProcessUtil {
+    @Getter
+    private static HoverTextAPI hoverTextAPI = new HoverTextAPI();
+
+    public static void processSign(SignChangeEvent e) {
+        if (e.getLine(0).equalsIgnoreCase(Config.getString(ConfigType.SETTING_LOCKSYMBOL)) && !e.getLine(1).isEmpty()) {
+            processNormalMsg(e);
+            processConditions(e);
+        }
+        LockData.getPlayerSignLocation().remove(e.getPlayer());
+        LockData.getPlayerDoorLocation().remove(e.getPlayer());
+    }
+
+    private static void processNormalMsg(SignChangeEvent e) {
+        /*如果世界不匹配,返回*/
+        if (!LocationUtil.checkWorld(e.getBlock().getLocation())) {
+            return;
+        }
+        /*替换第一行Symbol*/
+        e.setLine(0, BasicUtil.convert(Config.getString(ConfigType.SETTING_SYMBOLREPLACE)));
+
+        /*替换第四行为玩家名*/
+        e.setLine(3, "§l" + e.getPlayer().getName());
+
+        /*替换第二行价格*/
+        e.setLine(1, BasicUtil.convert(Config.getString(ConfigType.SETTING_CHARGE) + e.getLine(1)));
+    }
+
+    private static void processConditions(SignChangeEvent e) {
+
+        StringBuffer buffer = new StringBuffer();
+        String line = e.getLine(2);
+        boolean direction = false;
+
+        String flagEnter = Config.getString(ConfigType.SETTING_FLAGENTER);
+        String enterReplace = BasicUtil.convert(Config.getString(ConfigType.SETTING_ENTERREPLACE));
+
+        String flagLeave = Config.getString(ConfigType.SETTING_FLAGLEAVE);
+        String leaveReplace = BasicUtil.convert(Config.getString(ConfigType.SETTING_LEAVEREPLACE));
+
+        String flagEmpty = Config.getString(ConfigType.SETTING_FLAGEMPTY);
+        String emptyReplace = BasicUtil.convert(Config.getString(ConfigType.SETTING_EMPTYREPLACE));
+
+        String flagMoney = Config.getString(ConfigType.SETTING_FLAGMONEY);
+
+        String flagEffect = Config.getString(ConfigType.SETTING_FLAGEFFECT);
+        String effectReplace = BasicUtil.convert(Config.getString(ConfigType.SETTING_EFFECTREPLACE));
+        if (line.contains(flagEnter)) {
+            buffer.append(enterReplace);
+            direction = true;
+        }
+
+        if (line.contains(flagLeave)) {
+            buffer.append(leaveReplace);
+            direction = true;
+        }
+
+        /*开启默认双向*/
+        if (!direction) {
+            buffer.append(enterReplace + leaveReplace);
+        }
+
+        e.setLine(2, buffer.toString());
+        buffer.setLength(0);
+
+        /*背包为空条件*/
+        if (line.contains(flagEmpty)) {
+            buffer.append(emptyReplace);
+        }
+
+        /*余额条件*/
+        if (line.contains(flagMoney)) {
+            int money = BasicUtil.ExtraceInt(line);
+            Map<String, Boolean> moneyDetail = LockUtil.getMoneydetail(line, 0, 0);
+            String symbol = moneyDetail.keySet().iterator().next();
+            buffer.append(BasicUtil.convert(BasicUtil.replace(Config.getString(ConfigType.SETTING_MONEYREPLACE), "%needmoney", symbol + money)));
+        }
+
+        /*药水效果条件*/
+        if (line.contains(flagEffect)) {
+            buffer.append(effectReplace);
+        }
+
+        Location location = e.getBlock().getLocation();
+        Bukkit.getScheduler().runTaskLater(Lock.getInstance(), () -> {
+            hoverTextAPI.saveText(location, buffer.toString());
+        }, 1);
+    }
+}
