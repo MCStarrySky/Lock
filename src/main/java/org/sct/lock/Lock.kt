6859acf535b46bfeccb0a2613c0e28a4bb477a9f
梦曦·花已落 <qq625924077@gmail.com>
2020-07-01 19:40:16 +0800

    [U] 小修小补

--- src/main/java/org/sct/lock/Lock.kt
+++ src/main/java/org/sct/lock/Lock.kt
@@ -34,7 +34,7 @@ class Lock : JavaPlugin() {
             CheckUpdate.check(Bukkit.getConsoleSender(), instance, "LovesAsuna", "ZDRlZWY4ZDZlMzIyNDExYjk3NThlMGNiN2ZmYzg3NTRiOGIwZDUzZA==")
         }
         saveDefaultConfig()
-        Bukkit.getPluginCommand("lock").executor = SubCommandHandler(instance, "Lock")
+        getCommand("lock").executor = SubCommandHandler(instance, "Lock")
         server.consoleSender.sendMessage("      ___       ___           ___           ___     ")
         server.consoleSender.sendMessage("     /\\__\\     /\\  \\         /\\  \\         /\\__\\")
         server.consoleSender.sendMessage("    /:/  /    /::\\  \\       /::\\  \\       /:/  /")
